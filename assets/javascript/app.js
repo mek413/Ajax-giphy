2394cf37919cc52f16271c73b90082433b88f1fd
Marcus Kelly <mek413@gmail.com>
2019-09-02 20:16:46 -0500

    Gifs are able to be clicked to animate or stop if it's already active

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,7 +33,18 @@ $(document).ready(function(){
                 $("#gifs-div").append(charDiv);
                 
             }
-            
+            $(".gif").on("click",function(){
+                var state = $(this).attr("data-state");
+        
+                if(state === 'still'){
+                    $(this).attr("src", $(this).attr('data-animate'));
+                    $(this).attr("data-state", "animate");
+                }
+                else{
+                    $(this).attr("src",$(this).attr('data-still'));
+                    $(this).attr("data-state", "still")
+                };
+            });
         });
     };
     $("#add-character").on("click", function(event){
